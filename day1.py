5d88f27311abbd9f6b3b8e5d2829bc92b73259ca
ac2917 <aditya.chaturvedi17@imperial.ac.uk>
2018-12-04 23:27:48 +0000

    forgot to do day 1 part 2, added now

--- day1.py
+++ day1.py
@@ -4,4 +4,21 @@ results = []
 with open('input_day1.txt', newline='') as inputfile:
     for row in csv.reader(inputfile):
         results.append(int(row[0]))
+## PART ONE
 print(sum(results))
+
+
+## PART TWO
+seen = [0]
+done = False
+iters = 0
+while done == False:
+    for i,result in enumerate(results):
+        calc = result + seen[i+iters]
+        if calc in seen:
+            done = True
+            print(calc)
+            break
+        else:
+            seen.append(result + seen[i+iters])
+    iters+=len(results)
